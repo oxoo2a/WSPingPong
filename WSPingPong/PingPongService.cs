dba8e98a296c667713874539f1b5110bfa33d964
David Würfel <david@devinmotion.de>
2013-01-29 18:54:14 +0100

    Implement first draft of PingPongService and Host
    * Implement first draft of the PingPongService (with Ball_In and
    Ball_Out)
    * Implement the self-hosting in the WSPingPongHost project (incl. MEX)

---
+++ WSPingPong/PingPongService.cs
@@ -0,0 +1,20 @@
+﻿namespace WSPingPong
+{
+    using System.Diagnostics;
+
+    public class PingPongService : IPingPongService
+    {
+    
+        public void Ball_In(string ball)
+        {
+            Debug.WriteLine("I got a ball: ({0})", ball);
+        }
+
+        public string Ball_Out()
+        {
+            const string ball = "BALL";
+            Debug.WriteLine("Here is your ball: ({0})", ball);
+            return ball;
+        }
+    }
+}
