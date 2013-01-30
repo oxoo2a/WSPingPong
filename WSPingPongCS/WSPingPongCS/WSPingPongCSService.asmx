e48ab36dfd3a857cdd7d630078e2cc4ce4db5f9e
Sheng, Li <raycarter@163.com>
2013-01-30 21:23:05 +0100

    WSPingPong C# webservice and client
    1. The folder WSPingPongCS contains a self-hosted C# web service
    implemented without interface. A new method bool ball_Check() is added
    to check that wether the service has a ball already. This method should
    be called by your own client before sending a ball to the service when
    you consider it as necessary too.
    2. The folder WSPingPongCSClient contains a C# client in which 2
    references for accessing the 2 web services are configured. The 2
    references should be reconfigured in your own test environment.

---
+++ WSPingPongCS/WSPingPongCS/WSPingPongCSService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WSPingPongCSService.asmx.cs" Class="WSPingPongCS.WSPingPongCSService" %>
---
+++ WSPingPongCS/WSPingPongCS/WSPingPongCSService.asmx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WSPingPongCS
+{
+    /// <summary>
+    /// Summary description for WSPingPongCSService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WSPingPongCSService : System.Web.Services.WebService
+    {
+        static string ball;
+        static int round = 0;
+        [WebMethod]
+        public void Ball_In(string ball_)
+        {
+            ball = ball_;
+        }
+        [WebMethod]
+        public string Ball_Out()
+        {
+            string ret=ball;
+            ball = "";
+            return ret;
+        }
+
+        // Wenn false, bedeutet, ein Ball ist noch da.
+        [WebMethod]
+        public bool Ball_Check()
+        {
+            if (ball != "" && ball.Length > 0)
+                return false;
+            else
+                return true;
+        }
+    }
+}
